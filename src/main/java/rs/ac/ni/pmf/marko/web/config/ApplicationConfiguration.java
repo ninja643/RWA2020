b18fa1e244142defc15cbf66ae26c7e0ab66343c
Marko Milosevic <ninja643@gmail.com>
2020-05-10 13:08:36 +0200

    Add ObjectMapper bean. Add REST controllers tests

--- src/main/java/rs/ac/ni/pmf/marko/web/config/ApplicationConfiguration.java
+++ src/main/java/rs/ac/ni/pmf/marko/web/config/ApplicationConfiguration.java
@@ -1,11 +1,23 @@
 package rs.ac.ni.pmf.marko.web.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan({ "rs.ac.ni.pmf.marko.web" })
 public class ApplicationConfiguration {
+
+	@Bean
+	public ObjectMapper getObjectMapper() {
+		final ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+		
+		return objectMapper;
+	}
 }
--- src/test/java/rs/ac/ni/pmf/marko/web/controller/TicketsRestControllerTest.java
+++ src/test/java/rs/ac/ni/pmf/marko/web/controller/TicketsRestControllerTest.java
@@ -1,8 +1,12 @@
 package rs.ac.ni.pmf.marko.web.controller;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.Arrays;
 import java.util.List;
@@ -13,6 +17,8 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -21,14 +27,21 @@ import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilde
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import lombok.extern.slf4j.Slf4j;
 import rs.ac.ni.pmf.marko.web.TestWebConfiguration;
+import rs.ac.ni.pmf.marko.web.controller.impl.ErrorController;
 import rs.ac.ni.pmf.marko.web.controller.impl.TicketsRestControllerImpl;
+import rs.ac.ni.pmf.marko.web.exception.ErrorInfo.ResourceType;
+import rs.ac.ni.pmf.marko.web.exception.ResourceNotFoundException;
 import rs.ac.ni.pmf.marko.web.model.api.TicketDTO;
 import rs.ac.ni.pmf.marko.web.service.TicketsService;
 
 @RunWith(SpringRunner.class)
 @WebAppConfiguration
 @ContextConfiguration(classes = { TestWebConfiguration.class })
+@Slf4j
 public class TicketsRestControllerTest {
 
 	@Mock
@@ -37,12 +50,18 @@ public class TicketsRestControllerTest {
 	@InjectMocks
 	private TicketsRestControllerImpl ticketsController;
 
+	@Autowired
+	private ObjectMapper objectMapper;
+
 	private MockMvc mockMvc;
 
 	@Before
 	public void initialize() {
 		MockitoAnnotations.initMocks(this);
-		mockMvc = MockMvcBuilders.standaloneSetup(ticketsController).build();
+		mockMvc = MockMvcBuilders
+				.standaloneSetup(ticketsController)
+				.setControllerAdvice(ErrorController.class)
+				.build();
 	}
 
 	@Test
@@ -69,8 +88,44 @@ public class TicketsRestControllerTest {
 		final MockHttpServletRequestBuilder request = MockMvcRequestBuilders.get("/tickets");
 
 		mockMvc.perform(request)
-				.andExpect(content().json(
-						"[{\"id\":1,\"username\":\"user1\",\"title\":\"Ticket 1\",\"description\":\"Ticket description 1\"},{\"id\":2,\"username\":\"user1\",\"title\":\"Ticket 2\",\"description\":\"Ticket description 2\"}]"))
+				.andExpect(content().json(objectMapper.writeValueAsString(tickets)))
 				.andDo(print());
+	}
+
+	@Test
+	public void shouldUpdateTicket() throws Exception {
+		final TicketDTO ticketDTO = TicketDTO.builder()
+				.id(10)
+				.username("user")
+				.title("title")
+				.description("description")
+				.build();
+
+		final String requestBody = objectMapper.writeValueAsString(ticketDTO);
+		log.info(requestBody);
+
+		final MockHttpServletRequestBuilder request = MockMvcRequestBuilders.put("/tickets/1")
+				.contentType(MediaType.APPLICATION_JSON_VALUE)
+				.content(requestBody);
+
+		mockMvc.perform(request)
+				.andExpect(status().isNoContent());
+
+		verify(ticketsService).updateTicket(anyInt(), any(TicketDTO.class));
+	}
+	
+	@Test
+	public void shouldHanldeExceptions() throws Exception
+	{
+		when(ticketsService.updateTicket(anyInt(), any(TicketDTO.class)))
+			.thenThrow(new ResourceNotFoundException(ResourceType.TICKET, "not found"));
+		
+		final MockHttpServletRequestBuilder request = MockMvcRequestBuilders.put("/tickets/1")
+				.contentType(MediaType.APPLICATION_JSON_VALUE)
+				.content("{}");
+		
+		mockMvc.perform(request)
+			.andExpect(status().isNotFound())
+			.andDo(print());
 	}
 }
