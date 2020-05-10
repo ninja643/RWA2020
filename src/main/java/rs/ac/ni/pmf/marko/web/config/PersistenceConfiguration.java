47fba05d52629db525c7ceee726bbf8e9b4da863
Marko Milosevic <ninja643@gmail.com>
2020-05-10 20:58:03 +0200

    Add PersistenceExceptionTranslationPostProcessor bean

--- src/main/java/rs/ac/ni/pmf/marko/web/config/PersistenceConfiguration.java
+++ src/main/java/rs/ac/ni/pmf/marko/web/config/PersistenceConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
@@ -63,5 +64,13 @@ public class PersistenceConfiguration {
 		final JpaTransactionManager transactionManager = new JpaTransactionManager();
 		transactionManager.setEntityManagerFactory(entityManagerFactory);
 		return transactionManager;
+	}
+
+	/*
+	 *	Used to convert the database exceptions to Spring's DataAccessException 
+	 */
+	@Bean
+	public PersistenceExceptionTranslationPostProcessor exceptionTranslation() {		
+		return new PersistenceExceptionTranslationPostProcessor();
 	}
 }
